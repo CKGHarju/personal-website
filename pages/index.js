5d2dea31054e3a4ab25b063e9bb83539466c73da
CKGHarju <christopher.harju@devex.com>
2020-06-07 20:21:02 +0300

    create header section

--- pages/index.js
+++ pages/index.js
@@ -1,151 +1,150 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import Head from '../components/head';
+import Head from 'next/head'
 import Nav from '../components/nav';
 import fetch from 'isomorphic-unfetch'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faMobileAlt } from '@fortawesome/free-solid-svg-icons'
 
-const Home = ({ renderDate }) => {
-  const [date, setDate] = useState(null);
-
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.json();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+const Home = () => {
 
   return (
     <div>
-      <Head title="Home" />
-      <Nav />
-      <div>Time of server side render: {renderDate}</div>
-
-      <div className="hero">
-        <h1 className="title">Hello world and such ..</h1>
-        <p className="description">
-          To get started, edit the <code>pages/index.js</code> or{' '}
-          <code>pages/api/date.js</code> files, then save to reload.
-        </p>
-
-        <p className="row date">
-          Time of client side render:&nbsp;{' '}
-          {date ? (
-            <span>
-              <b>{date.date}</b>
-            </span>
-          ) : (
-            <span className="loading"></span>
-          )}
-        </p>
-
-        <div className="row">
-          <Link href="https://github.com/zeit/next.js#setup">
-            <a className="card">
-              <h3>Getting Started &rarr;</h3>
-              <p>Learn more about Next.js on GitHub and in their examples.</p>
-            </a>
-          </Link>
-          <Link href="https://github.com/zeit/next.js/tree/master/examples">
-            <a className="card">
-              <h3>Examples &rarr;</h3>
-              <p>Find other example boilerplates on the Next.js GitHub.</p>
-            </a>
-          </Link>
-          <Link href="https://github.com/zeit/next.js">
-            <a className="card">
-              <h3>Create Next App &rarr;</h3>
-              <p>Was this tool helpful? Let us know how we can improve it!</p>
-            </a>
-          </Link>
+      <Head >
+        <title>Christopher Harju</title>
+      </Head>
+      <div className='page-container'>
+        <div className="top-section-container">
+          <div className="top-section">
+            <div className="top-section-text">
+              <h1>Christopher Harju</h1>
+              <h3>Fullstack software developer</h3>
+              <p className='long-text'>
+                Experienced working in the JavaScript eco-system.
+                Passionate about libraries such as, React, Redux and Express. I am a fast learner and a driven developer, I like being
+                challenged and work on tight schedules.
+              </p>
+            </div>
+            <div className="top-section-image">
+              <img className='profile-picture' src='images/christopher-harju.png' alt='profile picture of christopher harju'/>
+            </div>
+            <div className="top-section-contact">
+              <div className='contact-item'>
+                <FontAwesomeIcon icon={faGithub} size='lg'/>
+                <a className='contact-link' href="http://github.com/CKGHarju">github.com/CKGHarju</a>
+              </div>
+              <div className='contact-item'>
+                <FontAwesomeIcon icon={faLinkedin} size='lg'/>
+                <a className='contact-link' href="https://www.linkedin.com/in/christopher-harju-909b2362/">Linkedin</a>
+              </div>
+              <div className='contact-item'>
+                <FontAwesomeIcon icon={faEnvelope} size='lg'/>
+                <a className='contact-link' href="mailto:chrharju@gmail.com">chrharju@gmail.com</a>
+              </div>
+              <div className='contact-item'>
+                <FontAwesomeIcon icon={faMobileAlt} size='lg'/>
+                <a className='contact-link' href="tel:+358403724121">+358 40 372 4121</a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
       <style jsx>{`
-        .hero {
-          width: 100%;
-          color: #333;
-        }
-        .title {
+        :global(body) {
           margin: 0;
-          width: 100%;
-          padding-top: 80px;
-          line-height: 1.15;
-          font-size: 48px;
+          font-family: 'Ubuntu';
+          font-size: 16px;
         }
-        .title,
-        .description {
-          text-align: center;
+
+        h1 {
+          font-style: normal;
+          font-weight: 400;
         }
-        .row {
-          max-width: 880px;
-          margin: 80px auto 40px;
+
+        h3 {
+          font-style: normal;
+          font-weight: 400;
+        }
+
+        @media (min-width: 400px) and (max-width: 850px) {
+          /* For mobile phones: */
+          .long-text {
+            max-width: 400px;
+          }
+        }
+
+        @media (min-width: 851px) and (max-width: 1000px) {
+          /* For mobile phones: */
+          .long-text {
+            max-width: 600px;
+          }
+        }
+
+        @media only screen and (min-width: 1001px) {
+          /* For mobile phones: */
+          .long-text {
+            max-width: 700px;
+          }
+        }
+
+        /* desktop mode */
+        .page-container {
+          height: 100vh;
+          width: 100vw;
           display: flex;
-          flex-direction: row;
-          justify-content: space-around;
+          flex-direction: column;
         }
-        .date {
-          height: 24px;
-          max-width: calc(100% - 32px)
-          text-align: center;
+
+        .top-section-container {
+          background-color: black;
+          padding: 30px;
+        }
+
+        .top-section-contact {
+          width: 100vw;
           display: flex;
-          align-items: center;
-          justify-content: center;
-          padding: 0 16px;
+          justify-content: space-between;
+          flex-wrap: wrap;
         }
-        .date p {
-          text-align: center;
-        }
-        .date span {
-          width: 176px;
-          text-align: center;
-        }
-        @keyframes Loading {
-          0%{background-position:0% 50%}
-          50%{background-position:100% 50%}
-          100%{background-position:0% 50%}
-        }
-        .date .loading {
-          max-width: 100%;
-          height: 24px;
-          border-radius: 4px;
-          display: inline-block;
-          background: linear-gradient(270deg, #D1D1D1, #EAEAEA);
-          background-size: 200% 200%;
-          animation: Loading 2s ease infinite;
-        }
-        .card {
-          padding: 18px 18px 24px;
-          width: 220px;
-          text-align: left;
+
+        .contact-link {
+          padding-left: 10px;
           text-decoration: none;
-          color: #434343;
-          border: 1px solid #9b9b9b;
+          color: white;
         }
-        .card:hover {
-          border-color: #067df7;
+
+        .contact-item {
+          padding-top: 15px;
+          padding-right: 30px;
         }
-        .card h3 {
-          margin: 0;
-          color: #067df7;
-          font-size: 18px;
+
+
+        .top-section {
+          color: white;
+          display: flex;
+          justify-content: space-between;
+          flex-wrap: wrap;
         }
-        .card p {
-          margin: 0;
-          padding: 12px 0 0;
-          font-size: 13px;
-          color: #333;
+
+        .top-section-text {
+        }
+
+        .profile-picture {
+          border-radius: 50%;
+          height: 192px;
+          width: 192px;
+        }
+
+        .top-section-image {
+          padding-top: 20px;
+          padding-bottom: 20px;
+          align-self: center;
         }
       `}</style>
     </div>
   );
 };
-
-Home.getInitialProps = async () => {
-  const res = await fetch('https://worldtimeapi.org/api/timezone/Europe/Madrid')
-  const json = await res.json()
-  return { renderDate: json.utc_datetime }
-}
 
 export default Home;
